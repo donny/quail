28a6170f47f324dcac92a9fcb2c6c38dfa2e2926
Donny Kurniawan <donny.kurniawan@gmail.com>
2017-04-25 07:40:14 +1000

    Test Airtable code

--- handler.js
+++ handler.js
@@ -1,6 +1,15 @@
 'use strict';
 
 module.exports.status = (event, context, callback) => {
+
+  var Airtable = require('airtable');
+  var base = new Airtable({apiKey: ''}).base('appBwk4mgN0DonbUA');
+
+  base('Price').find('rec0hrEYf995F3WHD', function(err, record) {
+      if (err) { console.error(err); return; }
+      console.log(record);
+  });
+
   const response = {
     statusCode: 200,
     body: JSON.stringify({
